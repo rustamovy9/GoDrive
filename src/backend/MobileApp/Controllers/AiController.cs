15a3ce1dc7b9b108b4b8210dc72eb8e134001a67
rustamovy9 <yusufrustamov821@gmail.com>
2026-03-01 17:24:25 +0500

    commit

--- src/backend/MobileApp/Controllers/AiController.cs
+++ src/backend/MobileApp/Controllers/AiController.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Contracts.AI;
 using Application.Contracts.Repositories;
 using Application.DTO_s.AI;
+using Domain.Constants;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MobileApp.Controllers;
@@ -9,9 +10,13 @@ namespace MobileApp.Controllers;
 [Route("api/ai")]
 public sealed class AiController(IAiAssistantService aiAssistantService,ICarRepository carRepository) : BaseController
 {
+    int UserId =>
+        int.Parse(User.FindFirst(CustomClaimTypes.Id)?.Value
+                  ?? throw new UnauthorizedAccessException("UserId not found"));
     [HttpPost("chat")]
     public async Task<IActionResult> Chat([FromBody] AiAssistantRequest aiAssistantRequest)
     {
+        aiAssistantRequest.UserId = UserId;
         var cars = await carRepository.GetAvailableCarsAsync();
         
         var context = cars.Value!.Select(c => new CarAiContext
